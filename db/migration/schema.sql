89aa27d9ff4fac08e46f32f5e32db5485f04193a
amandeep <samandeepkaue34@gmail.com>
2024-07-13 09:55:05 -0400

    tables created

---
+++ db/migration/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+\c employees_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL REFERENCES role(id),
+    manager_id INTEGER REFERENCES employee(id)
+);

d2f7f24e1b74d518e4185e785532fc154744c105
amandeep <samandeepkaue34@gmail.com>
2024-07-15 11:48:13 -0400

    queries to add new employee, view all roles and employess added

--- db/queries.js
+++ db/queries.js
@@ -13,11 +13,40 @@ module.exports = {
     addRole: async(title, salary, departmentId) => {
         const res = await pool.query( 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3) RETURNING *',
             [title, salary, departmentId]);
-        return res.rows[0];
+        return res.rows;
     },
 
     getAllRoles: async() => {
         const res = await pool.query('SELECT * FROM role');
         return res.rows;
+    },
+    addNewEmployee: async(first_name, last_name, role_id, manager_id) => {
+        const res = await pool.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4) RETURNING *',
+        [first_name, last_name, role_id, manager_id]
+        ); return res.rows;
+    },
+    getEmployees: async() => {
+        const res = await pool.query('SELECT * FROM employee');
+        return res.rows;
+    },
+    getEmployeeById: async (id) => {
+        try {
+            const res = await pool.query('SELECT * FROM employee WHERE id = $1', [id]);
+            return res.rows[0];
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    },
+    updateEmployeeRole: async (employeeId, roleId) => {
+        try {
+            await pool.query(
+                'UPDATE employee SET role_id = $1 WHERE id = $2',
+                [roleId, employeeId]
+            );
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
     }
 }
